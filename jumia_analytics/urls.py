c4b6901c97bbfbc59477de3eef10780e82f64624
Mehdi <jladou1999@gmail.com>
2026-05-15 15:33:36 +0100

    Correction URL setup dans urls principal

--- jumia_analytics/urls.py
+++ jumia_analytics/urls.py
@@ -3,15 +3,14 @@ URL configuration for jumia_analytics project.
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from analytics.views import setup_railway  # ← AJOUTER CETTE IMPORTATION
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('analytics.urls')),
-    path('', include('analytics.urls')),  # Routes frontend
+    path('setup/', setup_railway, name='setup'),  # ← AJOUTER CETTE LIGNE
+    path('', include('analytics.urls')),
 ]
-
 # Servir les fichiers statiques en développement
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
